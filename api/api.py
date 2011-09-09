fa57ccd3b9b5d5547ee35362cc7d386be7a62198
Tadas <tadas@chartbeat.com>
2011-09-09 20:59:26 +0000

    sample top_speakers response

--- api/api.py
+++ api/api.py
@@ -17,9 +17,15 @@ class ChatStatsApi(object):
     @json_wrap
     def top_speakers(self, chatroom):
         db = mongo.get_db(chatroom)
+	about = db.about
 
         result = [
-            {'speaker' : 'tadas'}
+            {'speaker' : 'tadas', 'lines' : 10, 'words' : 123, 'frequency' : 15.6}
+            {'speaker' : 'heewa', 'lines' : 160, 'words' : 123, 'frequency' : 151.6}
+            {'speaker' : 'aytan', 'lines' : 210, 'words' : 1293, 'frequency' : 10.6}
+            {'speaker' : 'allan', 'lines' : 510, 'words' : 1253, 'frequency' : 15.6}
+            {'speaker' : 'mona', 'lines' : 10, 'words' : 1323, 'frequency' : 5.6}
+            {'speaker' : 'tony', 'lines' : 120, 'words' : 13423, 'frequency' : 15.6}
         ]
 	return {'top_speakers' : result}
 
