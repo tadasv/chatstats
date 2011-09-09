4df84ed62489546c088143b9bed06a2450b4b72a
eytan <eytan85@gmail.com>
2011-09-09 15:27:06 -0400

    updating mongo

--- mongo.py
+++ mongo.py
@@ -1,4 +1,21 @@
 import pymongo
 
-connection = pymongo.Connection('localhost', 27017)
-db = connection.chatstats
+connection = None
+
+def get_connection():
+    global connection
+    if not connection:
+        connection = pymongo.Connection('localhost', 27017)
+    return connection
+
+
+def get_db(chatroom):
+    conn = get_connection()
+    return conn[chatroom]
+
+
+def get_collection(db, username):
+    """
+    @param db: mongo db returned by get_db
+    """
+    return db[username]
