6478315774ed9bc584dfa2495d80f8138ac3fb59
Tadas <tadas@chartbeat.com>
2011-09-09 21:47:39 +0000

    api update

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@
 
 import cherrypy
 import json
+import pymongo
 import mongo
 import os.path
 
@@ -35,16 +36,12 @@ class ChatStatsApi(object):
     @json_wrap
     def top_speakers(self, chatroom, **kwargs):
         db = mongo.get_db(chatroom)
-	about = db.about
+        docs = db.speakers.find(sort=('count', pymongo.DESCENDING) limit=15)
 
-        result = [
-            {'speaker' : 'tadas', 'lines' : 10, 'words' : 123, 'frequency' : 15.6},
-            {'speaker' : 'heewa', 'lines' : 160, 'words' : 123, 'frequency' : 151.6},
-            {'speaker' : 'aytan', 'lines' : 210, 'words' : 1293, 'frequency' : 10.6},
-            {'speaker' : 'allan', 'lines' : 510, 'words' : 1253, 'frequency' : 15.6},
-            {'speaker' : 'mona', 'lines' : 10, 'words' : 1323, 'frequency' : 5.6},
-            {'speaker' : 'tony', 'lines' : 120, 'words' : 13423, 'frequency' : 15.6},
-        ]
+        result = []
+        for d in docs:
+            result.append({'speaker' : d['_id'], 'messages' : d['count'], 'words': d['words'], 'chars' : d['chars']})
+
 	return {'top_speakers' : result}
 
     top_speakers.exposed = True
